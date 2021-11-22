7652843b35919e886edb8858959654bbb14faa78
Joao Bitelo <joao.octavio.bitelo@gmail.com>
2021-11-22 20:12:47 -0300

    Recupera base do T1

--- src/Receiver/Receiver.java
+++ src/Receiver/Receiver.java
@@ -1,7 +1,79 @@
 package Receiver;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
 public class Receiver {
+	private static int timeoutSeconds = 1;
+	private static int serverPort = 9876;
+	private static DatagramPacket receivePacket;
+	private static String message = "";
+	private static InetAddress IPAddress;
+	private static int port;
+	private static DatagramSocket serverSocket;
+
 	public static void main(String[] args) {
-		
+		System.out.println("Iniciando...");
+		while (true) {
+			// Configura socket para o cliente
+			setClientSocket();
+			// Declara o pacote a ser recebido
+			declarePackage();
+			// Obtem endereco ip do servidor com o DNS
+			setIp();
+			// Recebe o pacote
+			receiveMessage();
+
+		}
+	}
+
+	private static void receiveMessage() {
+		// Recebimento de mensagem
+		try {
+			serverSocket.receive(receivePacket);
+		} catch (IOException e) {
+			System.out.println("Ocorreu um erro ao receber a mensagem");
+		}
+		// Mensagem enviada
+		message = new String(receivePacket.getData());
+		System.out.println("message: " + message);
+
+	}
+
+	private static void setIp() {
+		// Ip do cliente
+		try {
+			IPAddress = receivePacket.getAddress();
+		} catch (Exception e) {
+			System.out.println();
+		}
+		// Porta
+		port = receivePacket.getPort();
+
+	}
+
+	private static void declarePackage() {
+		byte[] receiveData = new byte[1024];
+		receivePacket = new DatagramPacket(receiveData, receiveData.length);
+
+	}
+
+	private static void setClientSocket() {
+		// DatagramSocket
+		try {
+			serverSocket = new DatagramSocket(serverPort);
+		} catch (SocketException e1) {
+			System.out.println("Ocorreu um erro ao setar o datagram");
+		}
+		// Configuração de timeout
+//		try {
+//			serverSocket.setSoTimeout(timeoutSeconds * 1000);
+//		} catch (SocketException e) {
+//			System.out.println("Ocorreu um erro ao setar o time out");
+//		}
+
 	}
 }
--- src/Sender/Sender.java
+++ src/Sender/Sender.java
@@ -1,7 +1,74 @@
 package Sender;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
 public class Sender {
+	private static int timeoutSeconds = 2;
+	private static int serverPort = 9876;
+	private static int clientPort = 9877;
+	private static InetAddress IPAddress;
+	private static DatagramSocket clientSocket;
+	private static String myName = "";
+	private static String message = "";
+	private static String question = "";
+	private static String lastReceivedMessage = "";
+	private static int maxAttempsToRegister = 10;
+
 	public static void main(String[] args) {
-		
+		System.out.println("Iniciando...");
+		// Configura socket para o cliente
+		setClientSocket();
+		// obtem endereco ip do servidor com o DNS
+		setIp();
+		// Começa a enviar os arquivos
+		start();
+	}
+
+	private static void start() {
+		while (true) {
+			break;
+		}
+		String sentence = "aaaa";
+		byte[] sendData = new byte[1024];
+		sendData = sentence.getBytes();
+		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, serverPort);
+		try {
+			clientSocket.send(sendPacket);
+		} catch (IOException e) {
+			System.out.println("erro durante envio do pacote ao servidor");
+			e.printStackTrace();
+		}
+	}
+
+	private static void setIp() {
+		try {
+			IPAddress = InetAddress.getByName("localhost");
+			System.out.println("Conectado ao servidor com sucesso");
+		} catch (UnknownHostException e) {
+			System.out.println("");
+			System.out.println("erro durante obtencao do ip do servidor");
+			e.printStackTrace();
+		}
+	}
+
+	private static void setClientSocket() {
+		boolean foundAvailablePort = false;
+		while (!foundAvailablePort) {
+			System.out.println("Tentando a porta: " + clientPort);
+			try {
+				clientSocket = new DatagramSocket(clientPort);
+				foundAvailablePort = true;
+				clientSocket.setSoTimeout(timeoutSeconds * 1000);
+			} catch (SocketException e) {
+				System.out.println("Erro! porta ja em uso");
+				clientPort++;
+			}
+		}
+		System.out.println("Porta livre encontrada");
 	}
 }
